d45d3e335b67d399728bf51a937acd65f7af1a52
natalie619 <ngallegos@mail.csuchico.edu>
2023-12-05 14:52:25 -0800

    back and forth enemy added

---
+++ Marshmallow Dungeon/Assets/Scripts/BackAndForthEnemy.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 12/05/2023
+//this script shows how the enemy will move back and forth.
+
+public class BackAndForth : MonoBehaviour
+{
+    public GameObject leftPoint;
+    public GameObject rightPoint;
+    private Vector3 leftPos;
+    private Vector3 rightPos;
+    public int speed;
+    public bool goingLeft;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftPos = leftPoint.transform.position;
+        rightPos = rightPoint.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    //this will make the enemy move left or right 
+    private void Move()
+    {
+        if (goingLeft)
+        {
+            if(transform.position.x <= leftPos.x)
+            {
+                goingLeft = false;
+            }
+            else
+            {
+                transform.position += Vector3.left * Time.deltaTime * speed;
+            }
+        }
+        else
+        {
+            if(transform.position.x >= rightPos.x)
+            {
+                goingLeft = true;
+            }
+            else
+            {
+                transform.position += Vector3.right * Time.deltaTime * speed;
+            }
+        }
+    }
+
+
+}
